e3ebb372e0632a0ab18c73b6d51f7cbf3401c30b
bbx <bbx@bbx.com>
2024-01-22 17:22:24 +0800

    1.4

--- products/maps.js
+++ products/maps.js
@@ -2,10 +2,12 @@ Mapping.brochure = no => `detail_${no.replace('-', '').toLowerCase()}`;
 Mapping.maps = {
     ...Mapping.maps,
     image: new Mapping(
+        'BXG-05', 'https://takaratomymall.jp/img/goods/5/4904810936114_d7b7e1db554f4b4f8afde656e6904180.jpg',
         'BXG-03', 'https://takaratomymall.jp/img/goods/3/4904810913344_79d06c0bdc8743569fad277a2be9a560.jpg',
         no => no.replace('-', '') + '_list'
     ),
     brochure: new Mapping(
+        'BXG-05', 'https://takaratomymall.jp/img/goods/BEYBLADE/img_4904810936114.jpg',
         'BXG-03', 'https://takaratomymall.jp/img/goods/BEYBLADE/img_4904810913344.jpg',
         'BX-21', no => ['p','y','o'].map(c => `${Mapping.brochure(no)}_${c}`),
         'BX-20', no => ['B','G','P'].map(c => `${Mapping.brochure(no)}${c}`),
@@ -17,9 +19,9 @@ Mapping.maps = {
     rare: new Mapping(
     ),
     rate: new Mapping(
-        'BXG-03', 'App 抽中後購買',
+        ['BXG-05','BXG-03'], 'App 内抽中後購買',
         ['BX-16'], '各款封入比例均等',
-        ['BX-14','BX-24'], '封入比例：01、02 各 3；04、05 各 4；03、06 各 5',
+        ['BX-24','BX-14'], '封入比例：01、02 各 3；04、05 各 4；03、06 各 5',
     ),
     oversize: {
         eng: new Mapping(
