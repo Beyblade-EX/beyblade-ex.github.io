3618fca2f301019a1cfed77d9d7d64f44f3d4366
bbx <bbx>
2024-10-10 15:08:03 +0800

    0

--- customize/sheet.js
+++ customize/sheet.js
@@ -1,10 +1,56 @@
 const canvas = Q('canvas');
 const ctx = canvas.getContext('2d');
 
+const App = () => {
+    Controls.show(null);
+    Q('form button', button => button.type = 'button');
+    App.events();
+    //App.picker();
+    Q('#layer label:only-of-type')?.click();
+}
+App.events = () => {
+    Object.assign(Q('#layer'), {
+        onchange: Layers.change,
+        onclick: ev => ev.target.id == 'create' ? Layers.create(ev) : ['up', 'down'].includes(ev.target.id) ? Layers.move(ev) : null,
+        onpointerdown: ev => ev.target.id == 'delete' && Layers.delete(ev)
+    });
+    Object.assign(Q('#control-image'), {
+        onchange: Controls.image,
+        onclick: ev => ev.target.popoverTargetElement && (ev.preventDefault(), Q('aside').showPopover())
+    });
+    Object.assign(Q('#control-color'), {
+        oninput: ev => ev.target.type == 'color' && Controls.get(ev),
+        onchange: Controls.get
+    });
+    Q('#type').onclick = ev => ev.target.tagName == 'BUTTON' && Controls.chooseType(ev);
+    Q('#control').onchange = Controls.get;
+    
+    Q('#export').onclick = Layers.export;
+    Q('#import').onchange = Layers.import;
+    Q('#download').onclick = () => {
+        let document, page;
+        PDFLib.PDFDocument.create().then(doc => {
+            document = doc;
+            page = doc.addPage(PDFLib.PageSizes.A4.sort((a, b) => b - a));
+            return doc.embedPng(canvas.toDataURL("image/png", 1.0));
+        }).then(image => {
+            let scaled = image.scale(.2427);
+            for (let i = 0; i <= 11; i++)
+                page.drawImage(image, {
+                    x: 84.5 + i % 2 * (20 + scaled.width),
+                    y: 488 - Math.floor(i / 2) * (12.5 + scaled.height),
+                    width: scaled.width,
+                    height: scaled.height,
+                });
+            return document.save();
+        }).then(doc => window.open(URL.createObjectURL(new Blob([doc], { type: 'application/pdf' }))))
+        .catch(er => console.error(er));
+    }    
+}
 const Controls = {
     show (what) {
-        Q('#type,#control,#control-image,#control-color', fieldset => fieldset.style.display = 'none');
-        what === 0 ? (Q('#type').style.display = 'flex') : what && Q(`#control,#control-${what}`, fieldset => fieldset.style.display = 'grid');  
+        Q('#type,[id|=control]', fieldset => fieldset.hidden = true);
+        what === 0 ? Q('#type').hidden = false : what && Q(`#control,#control-${what}`, fieldset => fieldset.hidden = false);  
     },
     reset () {
         Q('input[type=color]', input => input.value = '#000000');
@@ -27,14 +73,13 @@ const Controls = {
         Q('#layer').disabled = true;
         const reader = new FileReader();
         reader.readAsDataURL(ev.target.files[0]);
-        reader.onload = () => Images.load(reader.result)
-            .then(img => {
-                Layers.selected.dataset.image = img.id;
-                Layers.selected.Q('span,img').replaceWith(img);
-                Q('form').dispatchEvent(new Event('change'));
-                Q('#layer').disabled = false;
-                Draw();
-            });  
+        reader.onload = () => Images.load(reader.result).then(img => {
+            Layers.selected.dataset.image = img.id;
+            delete Images[Layers.selected.Q('img')?.id];
+            Layers.selected.Q('span,img').replaceWith(img);
+            Q('#layer').disabled = false;
+            Draw();
+        });  
     },
     chooseType (ev) {
         Layers.selected.dataset.type = ev.target.id;
@@ -43,6 +88,7 @@ const Controls = {
     }
 }
 const Layers = {
+    label: (dataset, img) => E('label', {dataset}, [img ?? E('span'), E('input', {type: 'radio', name: 'layer'})]),
     frame: E('img', {
         src: './frame.png',
         onload: function() {
@@ -56,9 +102,10 @@ const Layers = {
         Layers.selected = Q('#layer label:has(:checked)');
         Layers.selected.dataset.type ? Controls.put() : Controls.show(0);
     },
-    add (ev) {
-        Layers.selected = E('label', [E('span'), E('input', {type: 'radio', name: 'layer', checked: true})]);
-        Q('#layer label:first-of-type').before(Layers.selected);
+    create (ev) {
+        let label = Layers.label();
+        Q('#layer label:first-of-type').before(label);
+        label.click();
         Q('#delete').disabled = false;
         Controls.reset();
         Controls.show(0);
@@ -79,7 +126,7 @@ const Layers = {
         Draw();
     },
     export () {
-        let layers = [...document.querySelectorAll('#layer label')].map(label => {
+        let layers = [Q('#layer label')].flat().map(label => {
             let data = {...label.dataset};
             data.image &&= Images[data.image].src;
             return data;
@@ -94,11 +141,9 @@ const Layers = {
         reader.readAsText(ev.target.files[0]);
         reader.onload = () => {
             Q('#layer label', label => label.remove());
-            let label = (dataset, img) => E('label', {dataset}, [img ?? E('span'), E('input', {type: 'radio', name: 'layer'})]);
-
             Promise.all(JSON.parse(reader.result).map(dataset => dataset.image ? 
-                Images.load(dataset.image).then(img => label({...dataset, image: img.id}, img)) : 
-                label(dataset)
+                Images.load(dataset.image).then(img => Layers.label({...dataset, image: img.id}, img)) : 
+                Layers.label(dataset)
             )).then(labels => {
                 Q('#delete').after(...labels);
                 Q('#layer label:first-of-type').click();
@@ -116,10 +161,10 @@ const Draw = () => {
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, Layers.frame.width, Layers.frame.height);
 
-    [Q('#layer label')].flat().reverse().forEach((label, i) => {
+    [Q('#layer label')].flat().reverse().forEach(label => {
         ctx.save();
-        label.dataset.image && Draw.image(label, i);
-        (label.dataset.color1 || label.dataset.color2) && Draw.color(label, i);
+        label.dataset.type == 'image' && Draw.image(label);
+        label.dataset.type == 'color' && Draw.color(label);
         ctx.restore();
     });
     Draw.frame();
@@ -145,22 +190,21 @@ Object.assign(Draw, {
     },
     color (label) {
         let {gradient: type, angle, x, y, scale: s, pull: p, opacity} = label.dataset;
-        let colors = [1,2,3].map(i => Draw.color.format(label.dataset[`color${i}`], label.dataset[`opacity${i}`])).filter(c => c);
         ({x, y} = Draw.transform(s, p, angle, x, y));
 
         type ??= 'Linear';
-        let gradient;
-        if (type == 'Linear') 
-            gradient = ctx.createLinearGradient(x, 0, x + canvas.width, 0);
-        else if (type == 'Radial')
-            gradient = ctx.createRadialGradient(x + canvas.width/2, y + canvas.width/2, 0, x + canvas.width/2, y + canvas.width/2, canvas.width/2);
-        else if (type == 'Conic')
-            gradient = ctx.createConicGradient(-Math.PI/2, x + canvas.width/2, y + canvas.width/2);
+        let gradient = 
+            type == 'Linear' ?
+                ctx.createLinearGradient(x, 0, x + canvas.width, 0) :
+            type == 'Radial' ?
+                ctx.createRadialGradient(x + canvas.width/2, y + canvas.width/2, 0, x + canvas.width/2, y + canvas.width/2, canvas.width/2) :
+            type == 'Conic' ?
+                ctx.createConicGradient(-Math.PI/2, x + canvas.width/2, y + canvas.width/2) : null;
 
-        gradient.addColorStop(0, colors[0]);
+        let colors = [1,2,3].map(i => Draw.color.format(label.dataset[`color${i}`], label.dataset[`opacity${i}`])).filter(c => c);
         (colors.length === 1 || type == 'Conic') && colors.push(colors[0]);
-        colors.slice(1).forEach((c, i, ar) => gradient.addColorStop((i + 1) / ar.length, c));
-        label.style.background = `${type}-gradient(${colors.join(',')})`;
+        colors.forEach((c, i, ar) => gradient.addColorStop(i / (ar.length - 1), c));
+        label.style.background = `${type}-gradient(${colors.join(',')}),white`;
 
         ctx.globalAlpha = opacity ?? 1;
         ctx.fillStyle = gradient;
@@ -170,45 +214,3 @@ Object.assign(Draw, {
     }
 }); 
 Draw.color.format = (color, opacity) => color ? `rgba(${color.replaceAll(/[^#]{2}/g, c => parseInt(c, 16) + ',').substring(1)}${opacity ?? 1})` : null;
-
-Q('form button', button => button.type = 'button');
-Object.assign(Q('#layer'), {
-    onchange: Layers.change,
-    onclick: ev => ev.target.id == 'add' ? Layers.add(ev) : ['up', 'down'].includes(ev.target.id) ? Layers.move(ev) : null,
-    onpointerdown: ev => ev.target.id == 'delete' && Layers.delete(ev)
-});
-Object.assign(Q('#control-image'), {
-    onchange: Controls.image,
-    onclick: ev => ev.target.popoverTargetElement && (ev.preventDefault(), Q('aside').showPopover())
-});
-Object.assign(Q('#control-color'), {
-    oninput: ev => ev.target.type == 'color' && Controls.get(ev),
-    onchange: Controls.get
-});
-Q('#type').onclick = ev => ev.target.tagName == 'BUTTON' && Controls.chooseType(ev);
-Q('#control').onchange = Controls.get;
-
-Q('#export').onclick = Layers.export;
-Q('#import').onchange = Layers.import;
-Q('#download').onclick = () => {
-    let document, page;
-    PDFLib.PDFDocument.create()
-    .then(doc => {
-        document = doc;
-        page = doc.addPage(PDFLib.PageSizes.A4.sort((a, b) => b - a));
-        return doc.embedPng(canvas.toDataURL("image/png", 1.0));
-    })
-    .then(image => {
-        let scaled = image.scale(.2427);
-        for (let i = 0; i <= 11; i++)
-            page.drawImage(image, {
-                x: 84.5 + i % 2 * (20 + scaled.width),
-                y: 488 - Math.floor(i / 2) * (12.5 + scaled.height),
-                width: scaled.width,
-                height: scaled.height,
-            });
-        return document.save();
-    })
-    .then(doc => window.open(URL.createObjectURL(new Blob([doc], { type: 'application/pdf' }))))
-    .catch(er => console.error(er));
-}
